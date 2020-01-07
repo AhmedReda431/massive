929e4d402f24acca7a32ec3796fe55af46f73d44
Ahmed Reda <48368885+AhmedReda431@users.noreply.github.com>
2020-01-07 19:03:12 +0200

    add of js

---
+++ js/plugin.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $(window).on("scroll",function(){
+        var sc = $(window).scrollTop(),
+                df = $(".down-button").offset().top;
+        if(sc+200 >=df){
+            $("header").css({opacity:0})
+            $(".massivelyButton").slideDown(1000)
+        }else{
+            $("header").css({opacity:1});
+            $(".massivelyButton").fadeOut(1000);
+        }
+
+    });
+    $(window).on("scroll",function(){
+        var sc = $(window).scrollTop()
+        if(sc > 700){
+            $(".scrollButton").fadeIn(1000)
+        }else{
+            $(".scrollButton").fadeOut(1000)
+        }
+    })
+    $(".scrollButton").click(function(){
+        $("html,body").animate({scrollTop:0},1000,function(){
+            $(".scrollButton").fadeOut(1000)
+        })
+    })
+});
+
+$(document).ready(function(){
+    $("#downLink").click(function(){
+        $.scrollSmoothTo($("#down"));
+    });
+  });
